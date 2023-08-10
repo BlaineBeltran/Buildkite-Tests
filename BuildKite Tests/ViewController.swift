869d00db474f60e2a6681caf17cc9bb25c539020
BlaineBeltran <bbeltran1998@yahoo.com>
2023-08-10 16:00:05 -0500

    Updated background color to white

--- BuildKite Tests/ViewController.swift
+++ BuildKite Tests/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         title = "BuildKite"
-        view.backgroundColor = .black
+        view.backgroundColor = .white
     }
 
 
